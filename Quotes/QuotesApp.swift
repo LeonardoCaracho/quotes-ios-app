7c37d5f4f527be23e230c34e74aa5e6ecf4075ca
Leonardo Caracho <leonardo.caracho@partner.bmw.com.br>
2023-03-05 17:06:11 -0300

    Initial Commit

---
+++ Quotes/QuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuotesApp.swift
+//  Quotes
+//
+//  Created by Leonardo Caracho on 05/03/23.
+//
+
+import SwiftUI
+
+@main
+struct QuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
